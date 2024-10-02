a794c4979feff23d713c96aa6e9d8ef0ccb88aa3
david <david>
2024-10-02 17:15:15 +0200

    Entity

--- src/conference/entities/conference.entity.ts
+++ src/conference/entities/conference.entity.ts
@@ -1,4 +1,5 @@
 import { differenceInDays, differenceInHours } from "date-fns";
+import { Entity } from "../../core/entities/entity";
 
 type ConferenceProps = {
     id: string;
@@ -9,15 +10,9 @@ type ConferenceProps = {
     seats: number;
 };
 
-export class Conference {
+export class Conference extends Entity<ConferenceProps> {
     public initialState: ConferenceProps;
     public props: ConferenceProps;
-
-    constructor(data: ConferenceProps) {
-        this.initialState = { ...data };
-        this.props = { ...data };
-        Object.freeze(this.initialState);
-    }
 
     isTooClose(now: Date): boolean {
         return differenceInDays(this.props.startDate, now) < 3;
@@ -33,13 +28,5 @@ export class Conference {
 
     isTooLong(): boolean {
         return differenceInHours(this.props.endDate, this.props.startDate) > 3;
-    }
-
-    update(data: Partial<ConferenceProps>) {
-        this.props = { ...this.props, ...data };
-    }
-
-    commit(): void {
-        this.initialState = this.props;
     }
 }
